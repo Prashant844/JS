f6adb74bb2e882c05c2ab54176a6ac903bbc90f6
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-02-26 09:31:18 +0530

    Update welcome.js

--- JS Programs/welcome.js
+++ JS Programs/welcome.js
@@ -1,1 +1,9 @@
 
+// Welcome in the JavaScript web Scripting Language.
+ 
+ //print " Hello World " Using javaScript
+  
+     console.log('Hello World...')
+//Output will be show in developer tool option
+// for show the Output : press f12 function key on the keyboard.
+// and go into the console window.
