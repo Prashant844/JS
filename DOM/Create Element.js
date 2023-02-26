b9cb1e9a5cadeaf51a2e0470eb03f160b04474a0
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-02-26 09:59:58 +0530

    Update Create Element.js

--- DOM/Create Element.js
+++ DOM/Create Element.js
@@ -1,4 +1,5 @@
 let element = document.createElement('h1')
 element.innerHTML = "Welcome in the JavaScript Tutorial";
 
-console.log(element)
+console.log(element) // for show in the HTML format
+console.log(element.textContent) // for show in the text format
